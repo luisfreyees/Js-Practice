5cb4f068c43db771835fdf8486e6297915fb106e
Jorge Chavez <ljorchavez@gmail.com>
2023-06-15 13:27:20 -0600

    Added new methods

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hellow world - test1')
+function printName(name) {
+    console.log(name)
+}
+
+function multiply(x,y) {
+    console.log(x * y)
+}
+
+function sayHello() {
+    console.log("Hello ")
+}
+
+printName("Felipe")
+multiply(5,5)
+sayHello()
+
