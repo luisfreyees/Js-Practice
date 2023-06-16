86cd52d4564ab574e5a662466c8b9aa694298560
luisfreyees <luisfreyees@gmail.com>
2023-06-16 11:50:29 -0600

    Prueba Xor

---
+++ xorexer.js
@@ -0,0 +1,14 @@
+function xor(a, b) {
+
+    if ( a && !b) {
+        console.log('True')
+    }
+          
+    else {  
+         if ( a && b)
+         console.log('Falso')
+     }  
+         
+    }
+    
+    xor(5 , 7)
