dc8bd8f9529e5b976e2411d3701e773cf94edcbb
luisfreyees <luisfreyees@gmail.com>
2023-06-20 17:24:43 -0600

    This is my progress in the pipe excercise

--- Exercises/Pipe-problem/pipe-problem.js
+++ Exercises/Pipe-problem/pipe-problem.js
@@ -3,28 +3,25 @@ function pipeFix(numbers){
 var i = 0 
 var list = []
 
-while (i < (numbers.length)) {
+while (i !== (numbers.length)) {
 
-    if ( (numbers[i]) < (numbers[i+1]) ) {
+    if ((numbers[i]) < (numbers[i + 1]) && ((numbers[i + 1])=(numbers[i] + 1)))   {
         list[i] = numbers[i]
-
     }
     else {
-        list[i] = numbers[i+1] + 1
-
+        list[i] = numbers[i] + 1
     }
 
 i = i + 1
     
 }
 
+
 console.log('Vueltas ' + i)
-console.log(numbers)
+console.log('Los numeros son: '+ numbers)
 console.log(numbers.length)
 console.log(list)
 
-console.log(numbers[0])
-
 }
 
-pipeFix ([1,2,3,4,5,6])
+pipeFix ([2,3,5,6])
